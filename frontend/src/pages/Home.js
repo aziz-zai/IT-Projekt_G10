7c9567f5af829d9842ca962d162fdaa89df2e337
aziz-zai <am202@hdm-stuttgart.de>
2022-04-15 17:47:39 +0200

    test page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Home.css'
 import NavBar from '../components/NavBar'
+import test from '../images/test.svg'
 
 function Home() {
   return (
       <div>
     <NavBar/>
     <div className="test">
-       
+        <img src={test}></img>
+        <h1>Die Seite steht ist noch in Bearbeitung!</h1>
     </div>
     </div>
   )
