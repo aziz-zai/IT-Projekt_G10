5eedb2d38cf793a8f192b1eba7fbe270ba343450
aziz-zai <am202@hdm-stuttgart.de>
2022-04-16 22:32:25 +0200

    All User API frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react';
 import './Home.css'
 import NavBar from '../components/NavBar'
 import test from '../images/test.svg'
+import PropTypes from 'prop-types'
+import OneAPI from '../api/OneAPI'
 
-function Home() {
-  return (
+
+export class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    // Init state
+    this.state = {
+      users: [],
+      loadingInProgress: false,
+      loadingError: null,
+    };
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  /** gets the balance for this account */
+  getUsers = () => {
+    OneAPI.getAPI().getUsers().then(users =>
+      this.setState({
+        users: users,
+        loadingInProgress: false,
+        loadingError: null
+        
+      })).catch(e =>
+        this.setState({ // Reset state with error from catch 
+          users: null,
+          loadingInProgress: false,
+          loadingError: e
+        })
+      );
+
+    // set loading to true
+    this.setState({
+      loadingInProgress: true,
+      loadingError: null
+    });
+  }
+
+
+  render() {
+    return (
       <div>
     <NavBar/>
     <div className="test">
         <img src={test}></img>
         <h1>Die Seite steht ist noch in Bearbeitung!</h1>
+        <div>{this.state.users} {console.log('users:'+ this.state.users[1])}</div>
     </div>
     </div>
-  )
+    )
+  }
 }
 
 export default Home
