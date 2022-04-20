9d768f02b6fb661d48447f7509eefc6f729fbb39
harbin-tairi <ht022@hdm-stuttgart.de>
2022-04-20 17:56:27 +0200

    ergänzung aktivitätenMapper

--- src/server/db/AktivitätenMapper.py
+++ src/server/db/AktivitätenMapper.py
@@ -9,10 +9,85 @@ class AktivitätenMapper(Mapper):
     def __init__(self):
         super().__init__()
 
+    def find_all(self):
+        """Auslesen aller Kunde.
+
+        :return Eine Sammlung mit Customer-Objekten, die sämtliche Kunden
+                repräsentieren.
+        """
+        result = []
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT id, timestamp, bezeichnung, dauer, kapazität from aktivitäten")
+        tuples = cursor.fetchall()
+
+        for (id, timestamp, bezeichnung, dauer, kapazität) in tuples:
+            Aktivitäten = Aktivitäten(id=id, timestamp=timestamp, bezeichnung=bezeichnung, dauer=dauer, kapazität = kapazität)
+
+            result.append(Aktivitäten)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+    
+    def find_by_key(self, key):
+        """Suchen eines Benutzers mit vorgegebener User ID. Da diese eindeutig ist,
+        """
+
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT id, timestamp, bezeichnung, dauer, kapazität FROM user WHERE id={}".format(key)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id, timestamp, bezeichnung, dauer, kapazität) = tuples[0]
+            aktivitäten = aktivitäten()
+            id=id,
+            timestamp=timestamp,
+            bezeichnung=bezeichnung
+            dauer=dauer,
+            kapazität=kapazität,
+            result = aktivitäten
+        except IndexError:
+            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
+            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_bezeichnung(self, bezeichnung):
+        """Auslesen aller Aktivitäten anhand der bezeichnung.
+        """
+        result = []
+        cursor = self._cnx.cursor()
+        command = "SELECT id, timestamp, bezeichnung, dauer, kapazität FROM user WHERE bezeichnung LIKE '{}' ORDER BY bezeichnung".format(bezeichnung)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        for (id, timestamp, bezeichnung, dauer, kapazität) in tuples:
+            aktivitäten = aktivitäten
+            id=id,
+            timestamp=timestamp,
+            bezeichnung=bezeichnung,
+            dauer=dauer,
+            kapazität=kapazität 
+            result.append(aktivitäten)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+        
+
     def insert(self, aktivitäten: Aktivitäten) -> Aktivitäten:
         """Create user Object."""
         cursor = self._cnx.cursor()
-        cursor.execute("SELECT MAX(id) AS maxid FROM aktivitäten ")
+        cursor.execute("SELECT MAX(id) AS maxid FROM aktivitäten")
         tuples = cursor.fetchall()
 
         for (maxid) in tuples:
@@ -21,7 +96,7 @@ class AktivitätenMapper(Mapper):
             else:
                 aktivitäten.id = 1
         command = """
-            INSERT INTO aktivitäten (
+            INSERT INTO user (
                 id, timestamp, bezeichnung, dauer, kapazität
             ) VALUES (%s,%s,%s,%s,%s)
         """
@@ -35,6 +110,3 @@ class AktivitätenMapper(Mapper):
         self._cnx.commit()
 
         return aktivitäten
-
-   
-    
