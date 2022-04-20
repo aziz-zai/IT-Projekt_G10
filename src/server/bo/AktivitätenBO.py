f4b420b0f0a84ac6d96d0717973d33e44f9f396c
harbin-tairi <ht022@hdm-stuttgart.de>
2022-04-20 16:02:50 +0200

    AktivitätenBO erzgänzt

--- src/server/bo/AktivitätenBO.py
+++ src/server/bo/AktivitätenBO.py
@@ -9,36 +9,4 @@ class Aktivitäten(BusinessObject):
         self.dauer = dauer
         self.kapazität = kapazität
 
-    def get_bezeichnung(self):
-        """Auslesen der Bezeichnung der Aktivität."""
-        return self._bezeichnung
-
-    def set_bezeichnung(self, bezeichnung):
-        """Setzen der Bezeichnung der Aktivität."""
-        self._bezeichnung = bezeichnung
-
-    def get_dauer(self):
-        """Auslesen der Dauer der Aktivität."""
-        return self.dauer
-
-    def set_dauer(self, dauer):
-        """Setzen der Dauer der Aktivität."""
-        self._dauer = dauer 
-
-    def get_kapazität(self):
-        """Auslesen der Kapazität der Aktivität."""
-        return self.kapazität
-
-    def set_kapazität(self, kapazität):
-        """Setzen der Kapazität der Aktivität."""
-        self._kapazität = kapazität
-
-
-    @staticmethod
-    def from_dict(dictionary=dict()):
-        """Umwandeln eines Python dict() in einen Customer()."""
-        obj = Aktivitäten()
-        obj.set_bezeichnung(dictionary["bezeichnung"])  # eigentlich Teil von BusinessObject !
-        obj.set_dauer(dictionary["dauer"])
-        obj.set_kapazität(dictionary["kapazität"])
-        return obj
+        super().__init__(timestamp_=timestamp_,id_=id_)
