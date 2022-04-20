744d93e4ddb0ac45a595dcdd9fcbdeaf775eab10
harbin-tairi <ht022@hdm-stuttgart.de>
2022-04-20 16:44:00 +0200

    AktivitätenMapper ergänzt

--- src/server/db/AktivitätenMapper.py
+++ src/server/db/AktivitätenMapper.py
@@ -1,9 +1,40 @@
-from server.bo.UserBO import User
+from time import time
+from server.bo.AktivitätenBO import Aktivitäten
 from server.db.Mapper import Mapper
 
 
-class UserMapper(Mapper):
+class AktivitätenMapper(Mapper):
 
 
     def __init__(self):
         super().__init__()
+
+    def insert(self, aktivitäten: Aktivitäten) -> Aktivitäten:
+        """Create user Object."""
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT MAX(id) AS maxid FROM aktivitäten ")
+        tuples = cursor.fetchall()
+
+        for (maxid) in tuples:
+            if maxid[0] is not None:
+                aktivitäten.id = maxid[0] + 1
+            else:
+                aktivitäten.id = 1
+        command = """
+            INSERT INTO aktivitäten (
+                id, timestamp, bezeichnung, dauer, kapazität
+            ) VALUES (%s,%s,%s,%s,%s)
+        """
+        cursor.execute(command, (
+            aktivitäten.id,
+            aktivitäten.timestamp,
+            aktivitäten.bezeichnung,
+            aktivitäten.dauer,
+            aktivitäten.kapazität,
+        ))
+        self._cnx.commit()
+
+        return aktivitäten
+
+   
+    
