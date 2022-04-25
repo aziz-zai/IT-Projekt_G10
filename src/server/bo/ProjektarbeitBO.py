308ff5551e27f79ac57f247f407cdce62c3816e2
Quang Cuong Dong <99296880+qd006@users.noreply.github.com>
2022-04-25 13:53:01 +0200

    ProjektarbeitBO ergänzt

--- src/server/bo/ProjektarbeitBO.py
+++ src/server/bo/ProjektarbeitBO.py
@@ -2,8 +2,12 @@ from .BusinessObject import BusinessObject
 from datetime import datetime
 
 
+# Sobald Zeitintervall verfügbar, Superklasse umändern auf Zeitintervall!
+
 class Projektarbeit(BusinessObject):
-    def __init__(self, bezeichnung: str, timestamp: datetime = datetime.now(), id: int= 0):
+    def __init__(self, bezeichnung: str, zeitintervall_id: str, activity_id: str, timestamp: datetime = datetime.now(), id: int= 0):
 
         self.bezeichnung = bezeichnung
+        self.zeitintervall_id = zeitintervall_id
+        self.activity_id = activity_id
         super().__init__(timestamp=timestamp,id=id)
