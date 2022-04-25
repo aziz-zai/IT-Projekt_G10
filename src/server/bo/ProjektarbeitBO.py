dc94dd7ce36b2145e1c4ed31cf3571429e3378b9
Quang Cuong Dong <99296880+qd006@users.noreply.github.com>
2022-04-25 19:46:05 +0200

    ProjektarbeitBO aktualisiert, ZeitintervallBO erstellt, ZeitintervallMapper erstellt

--- src/server/bo/ProjektarbeitBO.py
+++ src/server/bo/ProjektarbeitBO.py
@@ -1,13 +1,12 @@
+from server.bo.ZeitintervallBO import Zeitintervall
 from .BusinessObject import BusinessObject
 from datetime import datetime
 
 
-# Sobald Zeitintervall verfügbar, Superklasse umändern auf Zeitintervall!
-
-class Projektarbeit(BusinessObject):
-    def __init__(self, bezeichnung: str, zeitintervall_id: str, activity_id: str, timestamp: datetime = datetime.now(), id: int= 0):
+class Projektarbeit(Zeitintervall):
+    def __init__(self, bezeichnung: str, zeitintervall_id: str, activity_id: str, start: int, ende: int, zeitdifferenz: float, timestamp: datetime = datetime.now(), id: int= 0):
 
         self.bezeichnung = bezeichnung
         self.zeitintervall_id = zeitintervall_id
         self.activity_id = activity_id
-        super().__init__(timestamp=timestamp,id=id)
+        super().__init__(timestamp=timestamp,id=id,start=start,ende=ende,zeitdifferenz=zeitdifferenz)
---
+++ src/server/bo/ZeitintervallBO.py
@@ -0,0 +1,11 @@
+from .BusinessObject import BusinessObject
+from datetime import datetime
+
+
+class Zeitintervall(BusinessObject):
+    def __init__(self, start: int, ende: int, zeitdifferenz: float, timestamp: datetime = datetime.now(), id: int= 0):
+
+        self.start = start
+        self.ende = ende
+        self.zeitdifferenz = zeitdifferenz
+        super().__init__(timestamp=timestamp,id=id,)
