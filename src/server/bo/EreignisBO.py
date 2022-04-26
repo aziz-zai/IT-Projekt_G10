2782c8a70f34e9948a37a3cc26008990a703819f
Elif Demir <99296895+ed032@users.noreply.github.com>
2022-04-26 14:42:13 +0200

    EreignisMapper gelöscht, EreignisBO überarbeitet, KommenBO erstellt

--- src/server/bo/EreignisBO.py
+++ src/server/bo/EreignisBO.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 class Ereignis(BusinessObject):
-    def __init__(self, timestamp: datetime = datetime.now(), id: int= 0, zeitpunkt: datetime = datetime.now()):
+    def __init__(self, timestamp: datetime = datetime.now(), id: int= 0, zeitpunkt: datetime = datetime):
 
         self.zeitpunkt = zeitpunkt
-        super().__init__(timestamp=timestamp,id=id)
+        super().__init__(timestamp = timestamp,id = id)
