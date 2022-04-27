3c1692d5c96828083c18c980203b2f005105a111
Quang Cuong Dong <99296880+qd006@users.noreply.github.com>
2022-04-27 13:40:50 +0200

    projektarbeitBO kleine änderungen

--- src/server/bo/ProjektarbeitBO.py
+++ src/server/bo/ProjektarbeitBO.py
@@ -3,9 +3,9 @@ from datetime import datetime
 
 
 class Projektarbeit(Zeitintervall):
-    def __init__(self, bezeichnung: str, zeitintervall_id: str, activity_id: str, start: int, ende: int, zeitdifferenz: float, timestamp: datetime = datetime.now(), id: int= 0):
+    def __init__(self, bezeichnung: str, zeitintervall: int, activity: int, start: int, ende: int, zeitdifferenz: float, timestamp: datetime = datetime.now(), id: int= 0):
 
         self.bezeichnung = bezeichnung
-        self.zeitintervall_id = zeitintervall_id
-        self.activity_id = activity_id
+        self.zeitintervall = zeitintervall
+        self.activity = activity
         super().__init__(timestamp=timestamp,id=id,start=start,ende=ende,zeitdifferenz=zeitdifferenz)
