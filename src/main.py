4fd3d0fd09bdb6cb1e5c3aebf4f4ac13252738de
harbin-tairi <ht022@hdm-stuttgart.de>
2022-05-23 14:51:53 +0200

    aktivitäten redundand int ID

--- src/main.py
+++ src/main.py
@@ -486,53 +486,6 @@ class AktivitätenOperations(Resource):
         aktd = adm.get_aktivitäten_by_id(id)
         adm.delete_aktivitäten(aktd)
         return '', 200
-"""""
-@projectone.route('/aktivitäten/<int:id>')
-@projectone.response(500, 'Falls es zu einem Server-seitigen Fehler kommt.')
-class AktivitätenListOperations(Resource):
-    @projectone.marshal_list_with(aktivitäten)
-    def get(self, id):
-        """Auslesen aller Customer-Objekte.
-
-        Sollten keine Customer-Objekte verfügbar sein, so wird eine leere Sequenz zurückgegeben."""
-        adm = Administration()
-        aktivitäten = adm.get_aktivitäten_by_id(id)
-        return aktivitäten
-
-    @projectone.marshal_with(aktivitäten)
-    def put(self, id):
-        """Update eines bestimmten aktivitäten-Objekts.
-
-        **ACHTUNG:** Relevante id ist die id, die mittels URI bereitgestellt und somit als Methodenparameter
-        verwendet wird. Dieser Parameter überschreibt das ID-Attribut des im Payload der Anfrage übermittelten
-        Customer-Objekts.
-        """
-        adm = Administration()
-        up = Aktivitäten(**api.payload)
-
-
-        if up is not None:
-            """Hierdurch wird die id des zu überschreibenden (vgl. Update) Account-Objekts gesetzt.
-            Siehe Hinweise oben.
-            """
-            up.id = id
-            adm.update_aktivitäten(up)
-            return '', 200
-        else:
-            return '', 500
-
-    @projectone.marshal_with(aktivitäten)
-    def delete(self, id):
-        """Löschen eines bestimmten User-Objekts.
-
-        Das zu löschende Objekt wird durch die ```id``` in dem URI bestimmt.
-        """
-        adm = Administration()
-
-        aktivitätend = adm.get_aktivitäten_by_id(id)
-        adm.delete_aktivitäten(aktivitätend)
-        return '', 200
-"""""
 
 @projectone.route('/aktivitäten/<int:project_id>')
 @projectone.response(500, 'Falls es zu einem Server-seitigen Fehler kommt.')
