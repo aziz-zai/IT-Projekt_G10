3f1534b3f560789a34a3cb9c857ef656f7af052d
Zadranm <80890668+Zadranm@users.noreply.github.com>
2022-05-24 16:36:00 +0200

    Project and Membership API in Frontend
    Signed-off-by: Zadranm <80890668+Zadranm@users.noreply.github.com>

--- frontend/src/api/OneAPI.js
+++ frontend/src/api/OneAPI.js
@@ -1,4 +1,6 @@
 import UserBO from './UserBO';
+import ProjectBO from './ProjectBO';
+import MembershipBO from './MembershipBO';
 
 
 /**
@@ -16,13 +18,44 @@ export default class OneAPI {
   // Local Python backend
   #OneServerBaseURL = '/projectone';
 
-  // Local http-fake-backend 
-  //#bankServerBaseURL = '/api/bank';
-
- 
-
   // User related
   #getUsersURL = (id) => `${this.#OneServerBaseURL}/users/${id}`;
+
+  // Project related
+  #getProjectsURL = (id) => `${this.#OneServerBaseURL}/projects/${id}`;
+  #addProjectsURL = (user) => `${this.#OneServerBaseURL}/projects/${user}`;
+  #updateProjectURL = (id) => `${this.#OneServerBaseURL}/projects/${id}`;
+  #deleteProjectURL = (id) => `${this.#OneServerBaseURL}/projects/${id}`;
+
+  //Membership related
+  #addMembershipURL = () => `${this.#OneServerBaseURL}/membership/`;
+  #getMembershipURL = (id) => `${this.#OneServerBaseURL}/membership/${id}`;
+  #getMembershipByProjectURL = (id) => `${this.#OneServerBaseURL}/membership/projects/${id}`;
+  #getMembershipByUserURL = (id) => `${this.#OneServerBaseURL}/membership/projects/user/${id}`;
+  #getMembershipByUserAndProjectURL = (id) => `${this.#OneServerBaseURL}/membership/projects/user/project/${id}`;
+  #updateMembershipURL = (id) => `${this.#OneServerBaseURL}/membership/${id}`;
+  #deleteMembershipURL = (id) => `${this.#OneServerBaseURL}/membership/${id}`;
+
+  //Aktivitäten related
+
+  //Arbeitszeitkonto related
+
+  //Projektarbeit related
+
+  //Zeitintervallbuchung related
+
+  //Ereignisbuchung related
+
+  //Ereignis related
+
+  //Kommen related
+
+  //Gehen related
+
+  //Pause related
+
+
+
 
   /** 
    * Get the Singelton instance 
@@ -35,7 +68,7 @@ export default class OneAPI {
     }
     return this.#api;
   }
-
+  
   /**
    *  Returns a Promise which resolves to a json object. 
    *  The Promise returned from fetch() won’t reject on HTTP error status even if the response is an HTTP 404 or 500. 
@@ -66,4 +99,172 @@ export default class OneAPI {
       })
     })
   }
+
+  getProject(id) {
+    return this.#fetchAdvanced(this.#getProjectsURL(id)).then((responseJSON) => {
+      let projectsBOs = ProjectBO.fromJSON(responseJSON);
+      // console.info(customerBOs);
+      return new Promise(function (resolve) {
+        resolve(projectsBOs);
+      })
+    })
+  }
+
+  addProject(projectBO) {
+    return this.#fetchAdvanced(this.#addProjectsURL(), {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(projectBO)
+    }).then((responseJSON) => {
+      // We always get an array of ProjectBOs.fromJSON, but only need one object
+      let responseProjectBO = ProjectBO.fromJSON(responseJSON)[0];
+      // 
+      return new Promise(function (resolve) {
+        resolve(responseProjectBO);
+        })
+    })
+  }
+   /**
+     * Updates a customer and returns a Promise, which resolves to a CustomerBO.
+     * 
+     * @param {ProjectBO} projectBO to be updated
+     * @public
+     */
+  updateProject(projectBO) {
+    return this.#fetchAdvanced(this.#updateProjectURL(projectBO.getID()), {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json, text/plain',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(projectBO)
+    }).then((responseJSON) => {
+      // We always get an array of CustomerBOs.fromJSON
+      let responseProjectBO = ProjectBO.fromJSON(responseJSON)[0];
+      // 
+      return new Promise(function (resolve) {
+        resolve(responseProjectBO);
+      })
+    })
+  }j
+
+  deleteProject(id) {
+    return this.#fetchAdvanced(this.#deleteProjectURL(id), {
+      method: 'DELETE'
+    }).then((responseJSON) => {
+      // We always get an array of CustomerBOs.fromJSON
+      let responseProjectBO = ProjectBO.fromJSON(responseJSON)[0];
+      // console.info(accountBOs);
+      return new Promise(function (resolve) {
+        resolve(responseProjectBO);
+      })
+    })
+  }
+
+  getMembership(id) {
+    return this.#fetchAdvanced(this.#getMembershipURL(id)).then((responseJSON) => {
+      let membershipBOs = MembershipBO.fromJSON(responseJSON);
+      // console.info(customerBOs);
+      return new Promise(function (resolve) {
+        resolve(membershipBOs);
+      })
+    })
+  }
+
+  getMembershipByProject(project) {
+    return this.#fetchAdvanced(this.#getMembershipByProjectURL(project)).then((responseJSON) => {
+      let membershipBOs = MembershipBO.fromJSON(responseJSON);
+      // console.info(customerBOs);
+      return new Promise(function (resolve) {
+        resolve(membershipBOs);
+      })
+    })
+  }
+
+  getMembershipByUser(user) {
+    return this.#fetchAdvanced(this.#getMembershipByUserURL(user)).then((responseJSON) => {
+      let membershipBOs = ProjectBO.fromJSON(responseJSON);
+      // console.info(customerBOs);
+      return new Promise(function (resolve) {
+        resolve(membershipBOs);
+      })
+    })
+  }
+
+  getMembershipByUserAndProject(user, project) {
+    return this.#fetchAdvanced(this.#getMembershipByUserAndProjectURL(user, project)).then((responseJSON) => {
+      let membershipBOs = ProjectBO.fromJSON(responseJSON);
+      // console.info(customerBOs);
+      return new Promise(function (resolve) {
+        resolve(membershipBOs);
+      })
+    })
+  }
+
+  addMembership(membershipBO) {
+    return this.#fetchAdvanced(this.#addMembershipURL(), {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(membershipBO)
+    }).then((responseJSON) => {
+      // We always get an array of ProjectBOs.fromJSON, but only need one object
+      let responseMembershipBO = ProjectBO.fromJSON(responseJSON)[0];
+      // 
+      return new Promise(function (resolve) {
+        resolve(responseMembershipBO);
+        })
+    })
+  }
+ /**
+     * Updates a membership and returns a Promise, which resolves to a MembershipBO.
+     * 
+     * @param {MembershipBO} membershipBO to be updated
+     * @public
+     */
+  updateMembership(membershipBO) {
+    return this.#fetchAdvanced(this.#updateMembershipURL(membershipBO.getID()), {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json, text/plain',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(membershipBO)
+    }).then((responseJSON) => {
+      // We always get an array of MembershipBOs.fromJSON
+      let responseMembershipBO = MembershipBO.fromJSON(responseJSON)[0];
+      // 
+      return new Promise(function (resolve) {
+        resolve(responseMembershipBO);
+      })
+    })
+  }
+
+  deleteMembership(membershipBO) {
+    return this.#fetchAdvanced(this.#deleteMembershipURL(membershipBO), {
+      method: 'DELETE'
+    }).then((responseJSON) => {
+      // We always get an array of MembershipBO.fromJSON
+      let responseMembershipBO = MembershipBO.fromJSON(responseJSON)[0];
+      //
+      return new Promise(function (resolve) {
+        resolve(responseMembershipBO);
+      })
+    })
+  }
+
+
+
+
+
+
+
+
+
+
 }
