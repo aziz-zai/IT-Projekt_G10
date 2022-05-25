4ebc3fc06888f338f6bcb2b8df575910e6410b13
Elif Demir <99296895+ed032@users.noreply.github.com>
2022-05-25 16:53:19 +0200

    Änderung 'Zeitintervallbuchung'

--- src/server/bo/ZeitintervallbuchungBO.py
+++ src/server/bo/ZeitintervallbuchungBO.py
@@ -4,9 +4,6 @@ from datetime import datetime
 
 class Zeitintervallbuchung(Buchung):
     def __init__(self, erstellt_von: int, erstellt_für: int, ist_buchung: bool, zeitintervall: int, zeitdifferenz:str="", timestamp: datetime = datetime.now(), id: int= 0):
-        self.erstellt_von = erstellt_von
-        self.erstellt_für = erstellt_für
-        self.ist_buchung = ist_buchung
         self.zeitintervall = zeitintervall
         self.zeitdifferenz = zeitdifferenz
         
