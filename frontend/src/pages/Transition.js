c4dd1ec8ee32e9186ab297e5ecc7fea7f4d5ea15
aziz-zai <aziz.z@gmx.de>
2022-06-07 18:49:13 +0200

    snackbar when updating profile

---
+++ frontend/src/pages/Transition.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class Transition extends Component {
+
+
+  render() {
+    return (
+      <div>Transition</div>
+    )
+  }
+}
+Transition.propTypes = {
+    
+  }
+export default Transition
