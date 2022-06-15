7a70a2792150c41d9ea67e3695cce79c601c800f
aziz-zai <aziz.z@gmx.de>
2022-06-15 21:09:27 +0200

    arbeitszeitkonto design

--- frontend/src/pages/Arbeitszeitkonto.js
+++ frontend/src/pages/Arbeitszeitkonto.js
@@ -2,13 +2,20 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import OneAPI from '../api/OneAPI'
 import './Arbeitszeitkonto.css'
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 export class Arbeitszeitkonto extends Component {
 
     constructor(props) {
         super(props);
     this.state = {
-      arbeitszeitKonto: null
+      arbeitszeitKonto: null,
+      ereignisbuchungSelected: false,
+      zeitintervallbuchungSelected: true
     };
   }
 
@@ -27,11 +34,24 @@ export class Arbeitszeitkonto extends Component {
     this.setState({
     });
   }
+
+handleEreignisbuchung = () => {
+  this.setState({
+    ereignisbuchungSelected: true,
+    zeitintervallbuchungSelected: false,
+  })
+}
+handleZeitintervallbuchung = () => {
+  this.setState({
+    ereignisbuchungSelected: false,
+    zeitintervallbuchungSelected: true,
+  })
+}
 componentDidMount(){
   this.loadArbeitszeitkonto();
 }
   render() {
-    const {arbeitszeitKonto} = this.state;
+    const {arbeitszeitKonto, ereignisbuchungSelected, zeitintervallbuchungSelected} = this.state;
     return (
       <div>
         {arbeitszeitKonto ? 
@@ -40,7 +60,27 @@ componentDidMount(){
        <div class="azkHeaderItem">Urlaubskonto: <strong>{arbeitszeitKonto[0].urlaubskonto} </strong>Tage</div>
        <div class="azkHeaderItem">Gleitzeit: <strong>{arbeitszeitKonto[0].gleitzeit}</strong> Stunden</div>
        </div>:null}
-       
+       <div class="buchungContainer">
+         <div class="buchungHeader">
+           <div class="buchungFilter"><FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label">Filter</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+        <FormControlLabel value="female" control={<Radio />} label="Ist Buchungen" />
+        <FormControlLabel value="male" control={<Radio />} label="Soll Buchungen" />
+      </RadioGroup>
+    </FormControl></div>
+           <button class="abwesenheitBtn"> Abwesenheit </button>
+         </div>
+         <div class="buchungSelection">
+           <div class="selectionItem1"><button class={zeitintervallbuchungSelected ? "selectionBtn" : "selectionBtnAlt"} onClick={this.handleZeitintervallbuchung}>Zeitintervallbuchungen</button></div>
+           <div class="selectionItem2"><button class={ereignisbuchungSelected ? "selectionBtn" : "selectionBtnAlt"} onClick={this.handleEreignisbuchung}>Ereignisbuchungen</button></div>
+         </div>
+         <div class="buchungen"></div>
+       </div>
       </div>
     )
   }
