f398244c5d4890c56cccb25242ff8a5127bf5904
aziz-zai <am202@hdm-stuttgart.de>
2022-06-18 20:17:31 +0200

    show ereignis of ereignisbuchung

--- frontend/src/components/Arbeitszeitkonto/EreignisbuchungListEntry.js
+++ frontend/src/components/Arbeitszeitkonto/EreignisbuchungListEntry.js
@@ -12,9 +12,99 @@ export class EreignisbuchungListEntry extends Component {
         super(props);
     this.state = {
         expandState: false,
+        ereignis: null,
+        ereignisYear: null,
+        ereignisMonth: null,
+        ereignisDay: null,
+        ereignisHour: null,
+        ereignisMinute: null,
+        ereignisSecond: null
     };
   }
-  
+
+  getKommenById = () => {
+    OneAPI.getAPI().getKommen(this.props.ereignisbuchung.ereignis).then(kommen =>{
+        const ereignisZeitpunkt = new Date(kommen[0].zeitpunkt)
+        const year = ereignisZeitpunkt.getFullYear()
+        const month = ereignisZeitpunkt.getMonth()
+        const day = ereignisZeitpunkt.getDate()
+        const hour = ereignisZeitpunkt.getHours()
+        const minute = ereignisZeitpunkt.getMinutes()
+        const seconds = ereignisZeitpunkt.getSeconds()
+        this.setState({
+        ereignis: kommen[0],
+        ereignisYear: year,
+        ereignisMonth: month,
+        ereignisDay: day,
+        ereignisHour: hour,
+        ereignisMinute: minute,
+        ereignisSecond: seconds
+      })}
+      ).catch(e =>
+        this.setState({ // Reset state with error from catch 
+        })
+      );
+    // set loading to true
+    this.setState({
+
+    });
+  }
+
+  getGehenById = () => {
+    OneAPI.getAPI().getGehen(this.props.ereignisbuchung.ereignis).then(gehen =>{
+        const ereignisZeitpunkt = new Date(gehen[0].zeitpunkt)
+        const year = ereignisZeitpunkt.getFullYear()
+        const month = ereignisZeitpunkt.getMonth()
+        const day = ereignisZeitpunkt.getDate()
+        const hour = ereignisZeitpunkt.getHours()
+        const minute = ereignisZeitpunkt.getMinutes()
+        const seconds = ereignisZeitpunkt.getSeconds()
+        this.setState({
+        ereignis: gehen[0],
+        ereignisYear: year,
+        ereignisMonth: month,
+        ereignisDay: day,
+        ereignisHour: hour,
+        ereignisMinute: minute,
+        ereignisSecond: seconds
+      })}
+      ).catch(e =>
+        this.setState({ // Reset state with error from catch 
+        })
+      );
+    // set loading to true
+    this.setState({
+
+    });
+  }
+  getEreignisById = () => {
+    OneAPI.getAPI().getEreignis(this.props.ereignisbuchung.ereignis).then(ereignis =>{
+        const ereignisZeitpunkt = new Date(ereignis[0].zeitpunkt)
+        const year = ereignisZeitpunkt.getFullYear()
+        const month = ereignisZeitpunkt.getMonth()
+        const day = ereignisZeitpunkt.getDate()
+        const hour = ereignisZeitpunkt.getHours()
+        const minute = ereignisZeitpunkt.getMinutes()
+        const seconds = ereignisZeitpunkt.getSeconds()
+        this.setState({
+        ereignis: ereignis[0],
+        ereignisYear: year,
+        ereignisMonth: month,
+        ereignisDay: day,
+        ereignisHour: hour,
+        ereignisMinute: minute,
+        ereignisSecond: seconds
+      })
+        }
+      ).catch(e =>
+        this.setState({ // Reset state with error from catch 
+        })
+      );
+    // set loading to true
+    this.setState({
+
+    });
+  }
   handleExpandState = () => {
     this.setState({
         expandState: !this.state.expandState,
@@ -22,14 +112,23 @@ export class EreignisbuchungListEntry extends Component {
   {console.log('expandState', this.state.expandState)}
   }
 
-  
+
 componentDidMount() {
+    if(this.props.ereignisbuchung.bezeichnung=='Arbeitsbeginn'){
+        this.getKommenById();
+    }
+    else if(this.props.ereignisbuchung.bezeichnung=='Arbeitsende'){
+        this.getGehenById();
+    }
+    else{
+        this.getEreignis();
+    }
 }
   render() {
       const {ereignisbuchung} = this.props;
-      const {expandState} = this.state;
+      const {expandState,  ereignis, ereignisYear, ereignisMonth, ereignisDay,ereignisHour, ereignisMinute, ereignisSecond} = this.state;
     return (
-      <div>{console.log('expandState', expandState)}
+      <div>{console.log('ereignis', ereignis)}
         <Accordion defaultExpanded={false} expanded={expandState} >
           <AccordionSummary
             expandIcon={<ExpandMoreIcon onClick={this.handleExpandState}/>}
@@ -57,6 +156,8 @@ componentDidMount() {
             </Grid>
           </AccordionSummary>
           <AccordionDetails>
+              { ereignis ? <div>{String(ereignisYear).padStart(4, "0")}-{String(ereignisMonth).padStart(2, "0")}-{String(ereignisDay).padStart(2, "0")} &nbsp;
+              {String(ereignisHour).padStart(2, "0")}:{String(ereignisMinute).padStart(2, "0")}:{String(ereignisSecond).padStart(2, "0")} </div>:null} sads
           </AccordionDetails>
         </Accordion>
       </div>
