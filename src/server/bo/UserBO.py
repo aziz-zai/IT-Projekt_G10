916e41675065bc57783b4d99e8aba9625a37c3a4
Quang Cuong Dong <99296880+qd006@users.noreply.github.com>
2022-06-21 19:43:59 +0200

    Comments für User

--- src/server/bo/UserBO.py
+++ src/server/bo/UserBO.py
@@ -1,5 +1,10 @@
 from .BusinessObject import BusinessObject
 
+"""
+Klasse User
+
+"""
+
 class User(BusinessObject):
 
     def __init__(self):
@@ -12,36 +17,47 @@ class User(BusinessObject):
         self._urlaubstage = 20
 
     def get_vorname(self):
+        """Auslesen des Vornamen"""
         return self._vorname
 
     def set_vorname(self, vorname):
+        """Setzen des Vornamen"""
         self._vorname = vorname
 
     def get_nachname(self):
+        """Auslesen des Nachnamen"""
         return self._nachname
 
     def set_nachname(self, nachname):
+        """Setzen des Nachnamen"""
         self._nachname = nachname
 
     def get_benutzername(self):
+        """Auslesen des Benutzernamen"""
         return self._benutzername
 
     def set_benutzername(self, benutzername):
+        """Setzen des Benutzernamen"""
         self._benutzername = benutzername
 
     def get_email(self):
+        """Auslesen der E-Mail"""
         return self._email
     
     def set_email(self, email):
+        """Setzen der E-Mail"""
         self._email = email
 
     def get_google_user_id(self):
+        """Auslesen der Google-User-ID"""
         return self._google_user_id
 
     def set_google_user_id(self, google_user_id):
+        """Setzen der Google-User-ID"""
         self._google_user_id = google_user_id
 
     def get_urlaubstage(self):
+        """Auslesen der Urlaubstage"""
         return self._urlaubstage
 
 
@@ -52,7 +68,7 @@ class User(BusinessObject):
 
     @staticmethod
     def from_dict(dictionary=dict()):
-        """Umwandeln eines Python dict() in User()."""
+        """Umwandeln eines Python dict() in ein User()."""
         obj = User()
         obj.set_vorname(dictionary["vorname"])
         obj.set_nachname(dictionary["nachname"])  
